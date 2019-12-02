1330cdce5d9668298889a0167aae41a3ede0e9b4
Zhansaya Sikhymbekova <zhansaia.sihimbekova@gmail.com>
2019-12-02 22:12:01 +0100

    edited profile model

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 
 class Profile(models.Model):
+    photo = models.ImageField(upload_to='media/% Y/% m/% d/', default='media/ava.png')
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     date_of_birth = models.DateTimeField(auto_now=False, auto_now_add=False)
+    not_show_age = models.BooleanField(default=False)
     email = models.EmailField(max_length=254)
     city = models.CharField(max_length=100)
     marriage_status = models.CharField(max_length=100)
-    pregnant = models.BooleanField(default=False)
-    gestation = models.DateTimeField(auto_now=False, auto_now_add=False)
-    sex_of_child = models.CharField(max_length=100)
